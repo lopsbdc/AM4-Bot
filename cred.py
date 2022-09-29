8d4bc7321dc6845597b10893fb66a41d71ea08c4
unknown <lopsbdc@gmail.com>
2022-09-29 02:14:35 -0300

    deply1

---
+++ cred.py
@@ -0,0 +1,6 @@
+token = '5772282041:AAELVrriYZ80BTP30V_vIjJGRhevHhK3S58'
+
+cred1 = 'botam4@hotmail.com'
+cred2 = 'Ys5qAaTReTLJa7F'
+cred3 = 'botam4@hotmail.com'
+cred4 = '163193915Mh?'
---
+++ main.py
@@ -0,0 +1,164 @@
+import os
+from selenium.common.exceptions import *
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+import logging
+import requests
+import cred
+from wordpress_xmlrpc import Client
+from wordpress_xmlrpc.methods import posts
+from wordpress_xmlrpc import WordPressPost
+
+#comandinhos pra startar o chrome
+
+def iniciar_drive():
+    chrome_options = Options()
+
+    arguments = [' --lang=en-US', 'window-size=1920,1080', '--incognito', '--disable-gpu', '--no-sandbox', '--headless']
+
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+    driver_path = os.environ.get('CHROMEDRIVER_PATH')
+
+    driver = webdriver.Chrome(executable_path=driver_path, options=chrome_options)
+
+    wait = WebDriverWait(
+        driver,
+        10,
+        poll_frequency=1,
+        ignore_exceptions=[
+            NoSuchElementException,
+            ElementNotVisibleException,
+            ElementNotSelectableException,
+        ]
+    )
+    return driver, wait
+
+#  Telegram
+
+token = cred.token
+
+def enviar_mensagem(chat_id, text, disable_notification=False):
+    data = requests.get(
+        f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}&disable_notifications={disable_notification}')
+    #print(data.json())
+
+#  Fim Telegram
+
+
+bot = 2
+
+while bot:
+
+    fuel = 'null'
+    co2 = 'null'
+
+    driver, wait = iniciar_drive()
+
+    #  Logando
+    driver.get('https://www.airlinemanager.com/')
+    menu = driver.find_element(By.XPATH, '/html/body/div[4]/div/div[2]/div[1]/div/button[2]')
+    menu.click()
+    time.sleep(15)
+    email = driver.find_element(By.XPATH, '//*[@id="lEmail"]')
+    email.send_keys(cred.cred1)
+    time.sleep(8)
+    senha = driver.find_element(By.XPATH, '//*[@id="lPass"]')
+    senha.send_keys(cred.cred2)
+    time.sleep(8)
+    login = driver.find_element(By.XPATH, '//*[@id="btnLogin"]')
+    login.click()
+    time.sleep(16)
+
+    #  Fuel
+
+    try:
+        botaofu = driver.find_element(By.ID, 'mapMaint')
+        time.sleep(15)
+        botaofu.click()
+        time.sleep(15)
+
+        fu = driver.find_element(By.XPATH, '//*[@id="fuelMain"]/div/div[1]/span[2]/b').text
+        fuele = fu.replace("$ ", "")
+        fuelee = fuele.replace(",", "")
+        fuel = int(fuelee)
+        logging.warning('Preço do combustível obtido com sucesso!')
+
+    except:
+        logging.warning('Erro ao obter preço do combustível')
+
+       #  CO2
+
+    try:
+        time.sleep(12)
+        botaoco = driver.find_element(By.XPATH, '//*[@id="popBtn2"]')
+        botaoco.click()
+        time.sleep(15)
+        co = driver.find_element(By.XPATH, '//*[@id="co2Main"]/div/div[2]/span[2]/b').text
+        co2e = co.replace("$ ", "")
+        co2ee = co2e.replace(",", "")
+        co2 = int(co2ee)
+        logging.warning('Preço do CO2 obtido com sucesso!')
+        driver.quit()
+
+    except:
+        logging.warning('Erro ao obter preço do CO2')
+
+
+
+
+    #  Hora dos avisos
+
+
+    if fuel < 1000 or co2 < 150:
+
+        # Telegram
+
+        try:
+            texto = (f'Preço do combustível: $ {fuel}, e do CO2: $ {co2}.')
+            enviar_mensagem(chat_id='-853345222', text=texto)
+            logging.warning('Informado no telegram com sucesso')
+
+        except:
+            logging.warning('Erro ao postar no telegram')
+
+        #  Wordpress
+
+        try:
+            TITULO = 'Atualização de preço'
+            TEXTO = (f'Preço do combustível: $ {fuel}, e do CO2: $ {co2}.')
+
+            your_blog = Client('https://www.mafiabrasil.com/xmlrpc.php', cred.cred3, cred.cred4)
+
+            myposts = your_blog.call(posts.GetPosts())
+
+            post = WordPressPost()
+            post.title = TITULO
+            post.slug = ''
+            post.content = TEXTO
+            post.id = your_blog.call(posts.NewPost(post))
+            post.post_status = 'publish'
+            your_blog.call(posts.EditPost(post.id, post))
+            logging.warning('Postado no Wordpress com sucesso!')
+
+        except:
+            logging.warning('Erro ao postar no wordpress')
+
+    elif fuel == 'null' or co2 == 'null':
+        driver.quit()
+
+    else:
+        logging.warning('valores ainda estão muito altos')
+
+
+    #  Refresh do bot
+    logging.warning('Bot fará uma nova busca em 10 segundos')
+    time.sleep(10)
+
